930b0f2807713096bf1b036c02a68fbe05d6e1b2
Stéphan Gaud <stephgoeseast@gmail.com>
2018-10-22 17:39:31 +0100

    BOILERPLATE

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds139193.mlab.com:39193/blog_dev`,
   cookieKey: '123123123'
 };
